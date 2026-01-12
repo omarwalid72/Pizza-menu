fb3e07849a193d4cd66a1d492bf04ffae941031b
omarwalid72 <owalid193@gmail.com>
2026-01-12 17:00:21 +0200

    Refactor Menu and Pizza components to display pizza details dynamically

--- src/index.js
+++ src/index.js
@@ -74,17 +74,24 @@ function Menu() {
         photoName="pizzas/margherita.jpg"
         price={10}
       />
+      <Pizza
+        name="Pizza Margherita"
+        ingredients="Bread with italian olive oil and rosemary"
+        photoName="pizzas/margherita.jpg"
+        price={10}
+      />
     </main>
   );
 }
-function Pizza(props) {
-  console.log(props);
-  
+function Pizza(props) {  
   return (
-    <div>
-      <img src="pizzas/margherita.jpg" alt="Pizza Margherita" />
-      <h3>Pizza Margherita</h3>
-      <p>Tomato and mozarella</p>
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price} €</span>
+      </div>
     </div>
   );
 }
